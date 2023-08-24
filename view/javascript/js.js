60915c355cb3e4c991a1b8dff3253692858a42e4
Ângelo Mário <107640956+angelomario@users.noreply.github.com>
2023-08-24 14:54:36 +0100

    Terminando o theme mode

--- view/javascript/js.js
+++ view/javascript/js.js
@@ -1,34 +1,56 @@
 // Dark and light mode
+let send = document.getElementById("send");
+let dark ="dark",border_W = "border-w",border_b = "border-black", bg_d="bg-dark",bg_w="bg-light",btn_o_d ="btn-outline-dark", transit="translate", light_shadow="light-shadow";
+let bola = document.querySelector(".role");
+let quadro = document.querySelector(".forma");
+let dado = document.querySelectorAll(".valor");
+let select = document.querySelectorAll(".seletion");
+let opt = document.querySelectorAll(".seletion>option");
+
+
 document.querySelector(".mode").addEventListener("click",()=>{
-    let send = document.getElementById("send");
-    let bola = document.querySelector(".role");
-    let quadro = document.querySelector(".forma");
-    let dado = document.querySelectorAll(".valor");
-    let select = document.querySelectorAll(".seletion");
+    mode();
+});
+function mode(){
     if(bola.getAttribute("class") == "role"){
-        send.classList.remove("btn-outline-dark");
-
-        document.body.classList.add("dark");
-        bola.classList.add("translate");
-        quadro.classList.add("dark");
-        quadro.classList.remove("bg-light");
-
-        select[0].classList.add("border-w");
-        select[1].classList.add("border-w");
-        dado[0].classList.remove("border-black");
-        dado[1].classList.remove("border-black");
-        dado[0].classList.add("border-w");
-        dado[1].classList.add("border-w");
-
-        send.classList.add("border-w");
+        send.classList.remove(btn_o_d);
+        document.body.classList.add(dark);
+        send.classList.add(border_W);
+        bola.classList.add(transit);
+        quadro.classList.add(dark);
+        quadro.classList.remove(bg_w);
+        quadro.classList.add(light_shadow);
+        for (let i = 0; i <= 7; i++) {
+            opt[i].classList.add(dark); 
+        }
+        for (let x = 0; x <= 1; x++) {
+            dado[x].classList.remove(border_b);
+            dado[x].classList.add(border_W);
+            select[x].classList.add(border_W);
+        }
         
     }else{
-        bola.classList.remove("translate");
-        document.body.classList.remove("bg-dark");
+        bola.classList.remove(transit);
+        document.body.classList.remove(bg_d);
+
+        send.classList.add(btn_o_d);
+        document.body.classList.remove(dark);
+        send.classList.remove(border_W);
+        bola.classList.remove(transit);
+        quadro.classList.remove(dark);
+        quadro.classList.add(bg_w);
+        quadro.classList.remove(light_shadow);
+        for (let i = 0; i <= 7; i++) {
+            opt[i].classList.remove(dark); 
+        }
+        for (let x = 0; x <= 1; x++) {
+            dado[x].classList.add(border_b);
+            dado[x].classList.remove(border_W);
+            select[x].classList.remove(border_W);
+        }
 
     }
-});
-
+}
 
 // Configurando a API
 
