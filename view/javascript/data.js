792d7231c6ee2a48f6feec244a2d22677c11e7e2
Ângelo Mário <107640956+angelomario@users.noreply.github.com>
2023-10-03 20:03:10 +0100

    consumindo a api

--- view/javascript/data.js
+++ view/javascript/data.js
@@ -1,12 +1,18 @@
 let url="https://economia.awesomeapi.com.br/last/";
 let btn_load=document.querySelector("#send");
 btn_load.addEventListener("click",()=>{
-
+    let de = document.querySelector("#de").value, para=document.querySelector("#para").value;
     let moedas={
-        de: document.querySelector("#de").value,
-        para:document.querySelector("#para").value
+        de,
+        para,
+        money:de + para
     }
     url="https://economia.awesomeapi.com.br/last/"+moedas.de+"-"+moedas.para;
+    fetch(url).then((response)=>{
+        return response.json();
+    }).then((dados)=>{
+        console.log(dados.rates.high);
+    })
     console.log(url)
 });
 
